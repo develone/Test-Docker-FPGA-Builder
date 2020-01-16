ea28a8f9932f0594e4553eee4eb9a5e2e3edb0dd
Edward Vidal Jr <develone@sbcglobal.net>
2020-01-16 10:14:46 -0700

    rm git clone

--- build-catzip-bin.sh
+++ build-catzip-bin.sh
@@ -1,8 +1,9 @@
 #/bin/bash
 
-git clone https://github.com/develone/Test-Docker-FPGA-Builder.git
-cd Test-Docker-FPGA-Builder/catzip
+cd catzip
 rm -f catzip.asc catzip.blif catzip.bin
+
+yosys -l 'synth_ice40 -blif catzip.blif -json catzip.json -top toplevel' toplevel.v
 
 nextpnr-ice40 --seed 8 --freq 40 --hx8k --pcf catzip.pcf --json catzip.json --asc catzip.asc
 
